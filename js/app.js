605dc8be6b4a33c40c308388a078dca21d5b12e4
Allchorne3 <38890461+Allchorne3@users.noreply.github.com>
2019-05-09 12:40:35 +0100

    Full Change
    Website redesigned and complete

--- js/app.js
+++ js/app.js
@@ -1,29 +1,23 @@
 var hamburger = document.querySelector("#hamburger")
 var menu = document.querySelector(".menu")
 var mainContent = document.querySelector("#main")
-var catButton = document.querySelector(".categories-btn")
-var catClose = document.querySelector(".category-close")
+var navHeaders = document.querySelector(".nav-headers")
 var logoSpan = document.querySelector(".logo-span")
+var header = document.querySelector(".main-header")
+
+//----------------------------------------------------------//
+//FUNCTIONS
+function revealNav(){
+  menu.classList.toggle("close")
+  header.classList.toggle("nav-open")
+  navHeaders.classList.toggle("nav-reveal")
+  logoSpan.classList.toggle("span-reveal")
+
+}
+//---------------------------------------------------------//
 
 //reveal nav when clicking the hamburger menu
-hamburger.addEventListener("click", _ => {
-  menu.classList.toggle("close")
-  mainContent.classList.toggle("nav-open")
-  logoSpan.classList.toggle("span-reveal")
-})
-
-//reveal the categories when the button is clicked and bring "close" opacity up to 1
-catButton.addEventListener("click", _ => {
-  mainContent.classList.add("category-reveal")
-  catClose.classList.add("close-reveal")
-  catButton.style.pointerEvents = "none"
-})
-
-catClose.addEventListener("click", _ => {
-  mainContent.classList.remove("category-reveal")
-  catClose.classList.remove("close-reveal")
-  catButton.style.pointerEvents = "auto"
-})
+hamburger.addEventListener("click", revealNav)
 
 //-------------------------------------------------------------------------------------//
 // MEDIA QUERIES
@@ -31,7 +25,7 @@ catClose.addEventListener("click", _ => {
 var tablet = window.matchMedia("(max-width: 900px)")
 var phone = window.matchMedia("(max-width: 550px)")
 
-function mediaQueryMatch(x) {
+function mediaQueryMatch(tablet) {
   if (tablet.matches) { // If media query matches
     mainContent.classList.toggle("nav-open-tab")
   } else if(phone.matches) {
@@ -40,5 +34,46 @@ function mediaQueryMatch(x) {
     mainContent.classList.toggle("nav-open")
   }
 }
+
+
+//-----------------------------------------------------------------------//
+//JQUERY
+//Lightbox
+$(document).ready(function(){
+  $(".button").click(function(){
+    //store the value of datafilter into the variable called value
+    var value = $(this).attr("data-filter");
+    
+    if (value == "all") 
+    {
+      $(".filter").css("display", "block")
+    } 
+    else 
+    {
+      $(".filter").not("."+value).css("display", "none")
+      $(".filter").filter("."+value).css("display", "block")
+    }
+    
+    //add active class to clicked button
+    $("ul .button").click(function(){
+      $(this).addClass('active').siblings().removeClass('active')
+    })
+  })
+})
+
+//Header transparency on scroll
+$(window).scroll(function(){
+  var wScroll = $(this).scrollTop()
+
+  if(wScroll > $('.home-content').height()) {
+    $('.main-header').addClass("trans")
+  } else {
+    $('.main-header').removeClass("trans")
+  }
+
+  if(wScroll > $('.home-content').height()) {
+    $('.main-header').addClass("trans")
+  }
+})
 
 
